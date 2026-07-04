c3eb3d5e4d262dcf9eca9227f6e542b6e413c9b7
Amar <amardsalunkhe53@gmail.com>
2026-07-04 23:19:02 +0530

    formatted files

--- data.tf
+++ data.tf
@@ -1,17 +1,15 @@
 data "azurerm_resource_group" "rg" {
   name = "rg-terraform-github-demo"
 }
-
 data "azurerm_key_vault" "kv" {
-  name = "sectets-keyvault-96"
+  name                = "sectets-keyvault-96"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
-
 data "azurerm_key_vault_secret" "vm_username" {
-  name = "vm-admin-username"
+  name         = "vm-admin-username"
   key_vault_id = data.azurerm_key_vault.kv.id
 }
 data "azurerm_key_vault_secret" "vm_password" {
-  name = "vm-admin-password"
+  name         = "vm-admin-password"
   key_vault_id = data.azurerm_key_vault.kv.id
 }
